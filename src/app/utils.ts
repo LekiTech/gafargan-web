c9f6e6693da7aef74273295d95267cfd1d2e6679
Abdullah Shirinov <www.sar.ras@gmail.com>
2024-06-01 22:43:30 +0500

    fix:project-start-errors

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -49,7 +49,7 @@ export function toLowerCaseLezgi(
   lezgiString: string,
   options: { capitalize: boolean } = { capitalize: false },
 ) {
-  const lowerCased = lezgiString.toLowerCase().replaceAll(/(?<=[кптцчКПТЦЧ])[i1lӏ|!]/g, 'I');
+  const lowerCased = lezgiString?.toLowerCase().replaceAll(/(?<=[кптцчКПТЦЧ])[i1lӏ|!]/g, 'I');
   if (options.capitalize) {
     return lowerCased.charAt(0).toUpperCase() + lowerCased.slice(1);
   }
