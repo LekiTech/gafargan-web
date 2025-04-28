c714f08d5878bc7dd267d70ed7d089c9a5531e4f
Kamran Tadzjibov <ktadzjibov@gmail.com>
2025-04-29 00:27:31 +0200

    fixed ssl issue with digitalocean hosting and typeorm configs

--- ormconfig.ts
+++ ormconfig.ts
@@ -11,7 +11,23 @@ import { Language, Role } from './src/repository/entities/enums';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: process.env.DATABASE_URL, // Database URL from environment variable
+  // url: process.env.DATABASE_URL, // Database URL from environment variable
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT, 10) : undefined,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_NAME,
+  ssl:
+    process.env.DATABASE_SSL !== 'false'
+      ? process.env.CA_CERT
+        ? {
+            rejectUnauthorized: true,
+            ca: process.env.CA_CERT,
+          }
+        : {
+            rejectUnauthorized: false,
+          }
+      : false,
   synchronize: false, // Automatically sync the database (set to false in production)
   logging: false, // Optional, set to false in production
   entities: [Word, WordDetail, LangDialect, User, Source, Definition, SpellingVariant, Translation],
