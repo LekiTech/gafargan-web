f8717a41880c75e14f9b5b513d05f999fe2690b8
Kamran Tadzjibov <ktadzjibov@gmail.com>
2026-04-18 14:24:06 +0200

    fixed build errors, updated axios

--- src/app/[lang]/dashboard/page.tsx
+++ src/app/[lang]/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { getPaginatedWords, searchAdvanced } from '@repository/word.repository';
+import { searchAdvanced } from '@repository/word.repository';
 import { getSources } from '@repository/source.repository';
 import { redirect } from 'next/navigation';
 import { Routes } from '../../routes';
@@ -8,8 +8,11 @@ import { FoundDefinitionsList } from './dictionary/components/FoundDefinitionsLi
 import { initTranslations } from '@i18n/index';
 import { LangToId } from '@api/languages';
 import { toNumber } from '../../utils';
+import { PaginatedResponse } from '@repository/types.model';
+import { SourceModelType, STATE, WordModelExistingNestedType } from './models/proposal.model';
+import { mapWordToModelNestedType } from './models/proposal.mapper';
 
-const EmployeesCrudPage: FC<{ params: Params; searchParams: SearchParams }> = async ({
+const Dashboard: FC<{ params: Params; searchParams: SearchParams }> = async ({
   params,
   searchParams,
 }) => {
@@ -40,98 +43,39 @@ const EmployeesCrudPage: FC<{ params: Params; searchParams: SearchParams }> = as
     );
   }
 
+  const paginatedWordsModel: PaginatedResponse<WordModelExistingNestedType> = {
+    ...paginatedWords,
+    items: paginatedWords.items.map(mapWordToModelNestedType),
+  };
+
   // Temporary disabled in prod until finishd with development
   if (process.env.NODE_ENV === 'production') {
     redirect(`/${lang}/${Routes.UserSearchPage}`);
   }
   const sources = await getSources();
+  const sourceModels: SourceModelType[] = sources.map((source) => {
+    return {
+      state: STATE.UNCHANGED,
+      id: source.id,
+      name: source.name,
+      authors: source.authors ?? undefined,
+      // publicationYear: source.publicationYear ?? undefined,
+      // providedBy: source.providedBy ?? undefined,
+      // providedByUrl: source.providedByUrl ?? undefined,
+      // processedBy: source.processedBy ?? undefined,
+      // copyright: source.copyright ?? undefined,
+      // seeSourceUrl: source.seeSourceUrl ?? undefined,
+      // description: source.description ?? undefined,
+    };
+  });
 
-  // const words = await getPaginatedWords({
-  //   page: 0,
-  //   size: 100,
-  //   wordLangDialectId: 1,
-  //   definitionsLangDialectId: 25,
-  // });
   return (
-    // <div>Hello world!</div>
-    // <CustomPaginationActionsTable words={words} />
-    <FoundDefinitionsList lang={lang} paginatedWords={paginatedWords} sourceModels={sources} />
+    <FoundDefinitionsList
+      lang={lang}
+      paginatedWords={paginatedWordsModel}
+      sourceModels={sourceModels}
+    />
   );
 };
 
-export default EmployeesCrudPage;
-// import { FC } from 'react';
-// import { createTheme } from '@mui/material/styles';
-// import StickyNote2Icon from '@mui/icons-material/StickyNote2';
-// import { AppProvider, type Navigation } from '@toolpad/core/AppProvider';
-// import { DashboardLayout } from '@toolpad/core/DashboardLayout';
-// import { PageContainer } from '@toolpad/core/PageContainer';
-// import { Crud, DataModel, DataSource, DataSourceCache } from '@toolpad/core/Crud';
-// import { getMany, getOne, createOne, updateOne, deleteOne, validate } from './employees';
-
-// export interface Note extends DataModel {
-//   id: number;
-//   title: string;
-//   text: string;
-// }
-
-// // let notesStore: Note[] = [
-// //   { id: 1, title: 'Grocery List Item', text: 'Buy more coffee.' },
-// //   { id: 2, title: 'Personal Goal', text: 'Finish reading the book.' },
-// // ];
-
-// const notesCache = new DataSourceCache();
-
-// const CrudBasic: FC = () => {
-//   const title = 'Test title';
-//   const notesDataSource: DataSource<Note> = {
-//     fields: [
-//       { field: 'id', headerName: 'ID' },
-//       { field: 'title', headerName: 'Title', flex: 1 },
-//       { field: 'text', headerName: 'Text', flex: 1 },
-//     ],
-
-//     getMany,
-
-//     getOne,
-
-//     createOne,
-
-//     updateOne,
-
-//     deleteOne,
-
-//     validate,
-//   };
-//   // React.useMemo(() => {
-//   //   if (router.pathname === '/notes/new') {
-//   //     return 'New Note';
-//   //   }
-//   //   const editNoteId = matchPath('/notes/:noteId/edit', router.pathname);
-//   //   if (editNoteId) {
-//   //     return `Note ${editNoteId} - Edit`;
-//   //   }
-//   //   const showNoteId = matchPath('/notes/:noteId', router.pathname);
-//   //   if (showNoteId) {
-//   //     return `Note ${showNoteId}`;
-//   //   }
-
-//   //   return undefined;
-//   // }, [router.pathname]);
-
-//   return (
-//     <PageContainer title={title}>
-//       {/* preview-start */}
-//       <Crud<Note>
-//         dataSource={notesDataSource}
-//         dataSourceCache={notesCache}
-//         rootPath="/notes"
-//         initialPageSize={10}
-//         defaultValues={{ title: 'New note' }}
-//       />
-//       {/* preview-end */}
-//     </PageContainer>
-//   );
-// };
-
-// export default Crud;
+export default Dashboard;
